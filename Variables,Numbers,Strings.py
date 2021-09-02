a917aa6889e552e37f2ff059751dc0c9f19cb07c
guna8897 <89916946+guna8897@users.noreply.github.com>
2021-09-02 16:38:26 +0530

    Add files via upload

---
+++ Variables,Numbers,Strings.py
@@ -0,0 +1,50 @@
+# VARIABLES
+
+text = ("Good evening")
+print(text)
+
+#1 variable two message
+
+pet = ("dog")
+print(pet)
+pet = ("cat")
+print(pet)
+
+a = ("hi")
+a = ("hello")
+print(a)
+
+# Multiline strings
+
+a = '''HI welcome all to "GCIT" '''
+print(a)
+
+# Changing case
+my_name = "guna"
+print(my_name.title())
+print(my_name.upper())
+print(my_name.lower())
+
+#concatenation
+
+first_name = "guna"
+last_name = "seelan"
+full_name = first_name + last_name
+print(full_name.title())
+
+# whitespace
+print("good evening".title())
+print("\tgood evening".title())
+print("good \tevening".title())
+
+print("\nGood evening")
+print("Good \nevening")
+
+
+
+
+
+
+
+
+
